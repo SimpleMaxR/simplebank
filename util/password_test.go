1903f3430538359e6d589d2387597d6da94e1483
HugoHo <hugoho912@outlook.com>
2024-08-01 19:30:10 +0800

    add user api

---
+++ util/password_test.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomString(6)
+	hashedPassword, err := HashPassword(password)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+
+	err = CheckPassword(password, hashedPassword)
+	require.NoError(t, err)
+
+	wrongPassword := RandomString(6)
+	err = CheckPassword(wrongPassword, hashedPassword)
+	require.Error(t, err)
+
+	hashedPassword1, err := HashPassword(password)
+	hashedPassword2, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword2)
+	require.NotEqual(t, hashedPassword1, hashedPassword2)
+}
